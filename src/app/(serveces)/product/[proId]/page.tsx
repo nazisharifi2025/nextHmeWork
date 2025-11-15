30ca87301059afdcd26bc38392338150b72444d8
nazi sharifi <banonazisharifi@gmail.com>
2025-11-15 20:46:28 +0430

    Commit

--- src/app/(serveces)/product/[proId]/page.tsx
+++ src/app/(serveces)/product/[proId]/page.tsx
@@ -1,6 +1,17 @@
+import type{ Metadata } from 'next';
 import React from 'react'
-async function page({params}:{params:Promise<{proId:string}>}) {
+type Paramitr ={
+    params:Promise<{proId:string,reviws:string}>
+}
+export const generateMetadata = async({params}:Paramitr):Promise<Metadata>=>{
+     const {proId , reviws} = await params;
+     return{
+        title: `Product ${proId}`
+     };
+}
+async function page({params}:Paramitr) {
     const {proId} = await params;
+    
   return (
     <div  className='h-[90vh] flex justify-center items-center'>
         <div className="h-52 w-92 text-3xl bg-cyan-900 text-white flex  justify-center items-center rounded-md">
