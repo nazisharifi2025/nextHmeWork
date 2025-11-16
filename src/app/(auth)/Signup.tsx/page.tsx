3d4837872712933ad87472b450f5bd55cb0c0aad
nazi sharifi <banonazisharifi@gmail.com>
2025-11-16 11:43:59 +0430

    Commit

--- src/app/(auth)/Signup.tsx/page.tsx
+++ src/app/(auth)/Signup.tsx/page.tsx
@@ -1,5 +1,9 @@
-import React from 'react'
-
+import { title } from 'process'
+import React from 'react' 
+export const metadata = {
+  title: 'Signup Page',
+  description: 'This is the signup page',
+}
 function page() {
   return (
     <div  className='h-[70vh] fle justify-center items-center'>page Signup</div>
