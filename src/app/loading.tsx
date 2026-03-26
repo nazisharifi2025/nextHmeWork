2b79210e3ca279249e9c0b3c071e26b91e4b7ff1
nazi sharifi <banonazisharifi@gmail.com>
2026-03-26 16:04:10 +0430

    add style for loding page

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function loading() {
   return (
-    <div className=' h-screen w-full min-h-screen flex justify-center items-center bg-stone-50'></div>
+    <div className=' h-screen w-full min-h-screen flex justify-center items-center bg-stone-50'>
+        <div className=' h-32 w-32'></div>
+    </div>
   )
 }
 
